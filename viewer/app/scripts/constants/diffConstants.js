98865a1f9e187e42bf904e00a27a78af5da54dba
Ruurd Moelker <rrmoelker@gmail.com>
2015-06-07 12:40:37 +0200

    Basic wallabyjs setup and diffConstant using underscore constant function instead of custom method.

--- viewer/app/scripts/constants/diffConstants.js
+++ viewer/app/scripts/constants/diffConstants.js
@@ -1,18 +1,14 @@
 'use strict';
 
-angular.module('visualDiffViewerApp').constant('DIFF_STATUS_ACCEPTED', 'accepted')
+angular.module('visualDiffViewerApp')
+  .constant('DIFF_STATUS_ACCEPTED', 'accepted')
   .constant('DIFF_STATUS_REJECTED', 'rejected')
   .constant('DIFF_STATUS_PENDING', 'pending')
   .factory('diffConstants', function (DIFF_STATUS_ACCEPTED, DIFF_STATUS_REJECTED, DIFF_STATUS_PENDING) {
-      function createGetter(value) {
-        return function() {
-          return value;
-        }
-      }
       return {
-        accepted: createGetter(DIFF_STATUS_ACCEPTED),
-        rejected: createGetter(DIFF_STATUS_REJECTED),
-        pending: createGetter(DIFF_STATUS_PENDING),
-        all: createGetter([DIFF_STATUS_ACCEPTED, DIFF_STATUS_REJECTED, DIFF_STATUS_PENDING])
+        accepted: _.constant(DIFF_STATUS_ACCEPTED),
+        rejected: _.constant(DIFF_STATUS_REJECTED),
+        pending: _.constant(DIFF_STATUS_PENDING),
+        all: _.constant([DIFF_STATUS_ACCEPTED, DIFF_STATUS_REJECTED, DIFF_STATUS_PENDING])
       }
   });
